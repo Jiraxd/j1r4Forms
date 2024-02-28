e9eaa079af273d238ce90d956a49523af721af7e
Jiraxd <jiratvcz@seznam.cz>
2024-02-28 14:05:19 +0100

    done

--- src/app/(protected)/forms/main/page.tsx
+++ src/app/(protected)/forms/main/page.tsx
@@ -1,7 +1,10 @@
 import { auth } from "../../../../../auth";
+import { useRouter } from "next/navigation";
 
 const MainFormsPage = async() => {
     const session = await auth();
+    const router = useRouter();
+    if(session === null) router.push("/");
     return(
         <div>
         <span>Welcome back {session?.user?.name}!</span>
