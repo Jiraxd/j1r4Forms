4a4cdcb9ba24ea28700fa46cc0a82534411ba890
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2024-03-01 21:59:54 +0100

    fix main page, theming

--- src/app/(protected)/forms/main/page.tsx
+++ src/app/(protected)/forms/main/page.tsx
@@ -1,63 +1,25 @@
 "use client";
-import { useEffect, useState } from "react";
-import { auth } from "../../../../../auth";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { Moonicon, Sunicon } from "@/app/icons";
 import { EnterSettingsBTN } from "@/components/mainPage/settingsBTN";
+import { NavBarForms } from "@/components/forms/navbarForms";
+import { useEffect, useState } from "react";
 
-const MainFormsPage = async () => {
+const MainFormsPage = () => {
+  const [theme, setTheme] = useState<string>("dark");
   useEffect(() => {
     let localTheme = localStorage.getItem("theme");
     if (localTheme) setTheme(localTheme);
   }, []);
-  const [theme, setTheme] = useState<string>("dark");
-  const session = await auth();
-  const router = useRouter();
-  if (session === null) router.push("/");
+
+  const handleThemeChange = (newTheme: string) => {
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
   return (
     <main data-theme={theme}>
-      <div
-        className="fixed w-full top-0 bg-base-300"
-        style={{
-          zIndex: 200,
-        }}
-      >
-        <div className="flex w-full min-h-16 items-center">
-          <div className="flex">
-            <Image
-              src={session?.user?.image || "/images/default.webp"}
-              alt={"logo"}
-              width={32}
-              height={32}
-              className="ml-5"
-            />
-            <p className="ml-3 font-bold text-xl text-base-content">
-              {`Welcome back, ${session?.user?.name}`}
-            </p>
-          </div>
-          <div className="flex cursor-pointer gap-2 mx-auto">
-            <Moonicon color={theme === "dark" ? "white" : "black"} />
-            <input
-              type="checkbox"
-              value="light"
-              className="toggle theme-controller"
-              checked={theme === "light"}
-              onClick={() => {
-                setTheme(theme === "dark" ? "light" : "dark");
-                localStorage.setItem(
-                  "theme",
-                  theme === "dark" ? "light" : "dark"
-                );
-              }}
-            />
-            <Sunicon color={theme === "dark" ? "white" : "black"} />
-          </div>
-          <EnterSettingsBTN>
-            <button className="btn btn-secondary w-32 mr-5">Settings</button>
-          </EnterSettingsBTN>
-        </div>
-      </div>
+      <NavBarForms theme={theme} onThemeChange={handleThemeChange} />
     </main>
   );
 };
---
+++ src/components/forms/navbarForms.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { Moonicon, Sunicon } from "@/app/icons";
+import { EnterSettingsBTN } from "@/components/mainPage/settingsBTN";
+import { Session } from "next-auth";
+import { getAuth } from "../../../actions/getAuth";
+
+type Props = {
+  theme: string;
+  onThemeChange: (newTheme: string) => void;
+};
+
+export const NavBarForms = ({ theme, onThemeChange }: Props) => {
+  const [session, setSession] = useState<Session>();
+  useEffect(() => {
+    async function getxd() {
+      const sessionTMP = await getAuth();
+      if (sessionTMP !== null) setSession(sessionTMP);
+    }
+    getxd();
+  }, []);
+
+  return (
+    <div
+      className="fixed w-full top-0 bg-base-300"
+      style={{
+        zIndex: 200,
+      }}
+    >
+      <div className="flex w-full min-h-16 items-center">
+        <div className="flex">
+          <Image
+            src={session?.user?.image || "/images/default.webp"}
+            alt={"logo"}
+            width={32}
+            height={32}
+            className="ml-5"
+          />
+          <p className="ml-3 font-bold text-xl text-base-content">
+            {`Welcome back, ${session?.user?.name || "user"}`}
+          </p>
+        </div>
+        <div className="flex cursor-pointer gap-2 mx-auto">
+          <Moonicon color={theme === "dark" ? "white" : "black"} />
+          <input
+            type="checkbox"
+            value="light"
+            className="toggle theme-controller"
+            checked={theme === "light"}
+            onClick={() => {
+              onThemeChange(theme === "dark" ? "light" : "dark");
+            }}
+          />
+          <Sunicon color={theme === "dark" ? "white" : "black"} />
+        </div>
+        <EnterSettingsBTN>
+          <button className="btn btn-accent w-32 mr-5">Settings</button>
+        </EnterSettingsBTN>
+      </div>
+    </div>
+  );
+};
