94aa17ac9530677747b88494cc6c7a182318307c
Jiraxd <jiratvcz@seznam.cz>
2024-03-04 13:04:00 +0100

    spacing

--- src/app/(protected)/forms/main/page.tsx
+++ src/app/(protected)/forms/main/page.tsx
@@ -21,7 +21,7 @@ const MainFormsPage = () => {
   return (
     <main data-theme={theme}>
       <NavBarForms theme={theme} onThemeChange={handleThemeChange} />
-      <div className="flex min-h-screen flex-row flex-wrap gap-4 items-center mt-40">
+      <div className="flex min-h-screen flex-row flex-wrap gap-4 items-center mt-40 ml-auto mt-10">
       <FormPreview />
       </div>
     </main>
