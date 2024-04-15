445b917078787c6cbc6bb01c75374a58b142fdbb
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2024-04-15 19:28:53 +0200

    ??

--- actions/saveImageServer.ts
+++ actions/saveImageServer.ts
@@ -1,8 +1,9 @@
 "use server";
-import * as fs from 'fs';
-import path from 'path';
+import * as fs from "fs";
+import path from "path";
 
-export const saveImageServer = async(base64:string, name:string) => {
-    const filePath = path.join(process.cwd(),`public/${name}.webp`); 
-    await fs.promises.writeFile(filePath, base64, 'base64');
-}
+export const saveImageServer = async (base64: string, name: string) => {
+  const filePath = path.join(process.cwd(), `public/${name}.webp`);
+  console.error(fs.readdirSync(process.cwd()).toString());
+  await fs.promises.writeFile(filePath, base64, "base64");
+};
