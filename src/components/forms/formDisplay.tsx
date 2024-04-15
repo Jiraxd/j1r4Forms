f9d798869d058e2e1c7aecf3c07d80eabaee730e
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2024-04-15 20:04:44 +0200

    fix image vercel

--- src/components/forms/formDisplay.tsx
+++ src/components/forms/formDisplay.tsx
@@ -1,16 +1,25 @@
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { list } from "@vercel/blob";
+import { useEffect, useState } from "react";
+import { LoaderCircle } from "../loader";
 
 type Props = {
   formName: string;
   formID: string;
 };
 
-export const FormDisplay = async ({ formName, formID }: Props) => {
+export const FormDisplay = ({ formName, formID }: Props) => {
+  const [imageURL, setImageURL] = useState<string>(`/${formID}.webp`);
   const router = useRouter();
-  const response = await list();
-  console.log(response);
+  useEffect(() => {
+    async function getImage() {
+      const response = await list({ prefix: formID });
+      console.log(response);
+      setImageURL(response.blobs[0].url);
+    }
+    if (process.env.NODE_ENV === "production") getImage();
+  }, []);
   return (
     <button
       className="cursor-pointer flex-col items-center"
@@ -19,18 +28,29 @@ export const FormDisplay = async ({ formName, formID }: Props) => {
         `);
       }}
     >
-      <Image
-        src={
-          process.env.NODE_ENV === "production"
-            ? response.blobs.find((value) => value.pathname === formID)
-                ?.downloadUrl || ""
-            : `/${formID}.webp`
-        }
-        className="min-w-[256px]"
-        alt="logo"
-        width={256}
-        height={128}
-      />
+      {process.env.NODE_ENV === "production" ? (
+        <>
+          {imageURL === `/${formID}.webp` ? (
+            <LoaderCircle />
+          ) : (
+            <Image
+              src={imageURL}
+              className="min-w-[256px]"
+              alt="logo"
+              width={256}
+              height={128}
+            />
+          )}
+        </>
+      ) : (
+        <Image
+          src={imageURL}
+          className="min-w-[256px]"
+          alt="logo"
+          width={256}
+          height={128}
+        />
+      )}
       <div className="mt-4">{formName}</div>
     </button>
   );
