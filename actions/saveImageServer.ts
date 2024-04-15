1301e254639fc77166a0d0eec4a97e998c083191
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2024-04-15 19:00:31 +0200

    Fix path on vercel deployment

--- actions/saveImageServer.ts
+++ actions/saveImageServer.ts
@@ -1,7 +1,9 @@
 "use server";
 import * as fs from 'fs';
+import path from 'path';
 
 export const saveImageServer = async(base64:string, name:string) => {
-    const filePath = `./public/previews/${name}.webp`; 
+    const filePath = path.join(process.cwd(),`/public/previews/${name}.webp`); 
+    console.log(filePath);
     await fs.promises.writeFile(filePath, base64, 'base64');
 }
