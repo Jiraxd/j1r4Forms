70791f001095e613b5fc8393b778751fb9192210
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2024-04-15 19:19:41 +0200

    vercel test

--- actions/saveImageServer.ts
+++ actions/saveImageServer.ts
@@ -3,8 +3,7 @@ import * as fs from 'fs';
 import path from 'path';
 
 export const saveImageServer = async(base64:string, name:string) => {
-    if(!fs.existsSync(path.join(process.cwd(),'/public/previews/')))
-        await fs.promises.mkdir(path.join(process.cwd(),'/public/previews/'), { recursive: true });
-    const filePath = path.join(process.cwd(),`/public/previews/${name}.webp`); 
+    const filePath = path.join(process.cwd(),`public/${name}.webp`); 
+    console.log(fs.readdirSync(process.cwd()))
     await fs.promises.writeFile(filePath, base64, 'base64');
 }
